ec95fe6cf02074a5673d6225c152be18720e5bd5
Khadija <memmedlixedice318@gmail.com>
2024-12-15 18:32:36 +0400

    restaurant_app created

--- React-task1/src/assets/components/FoodCard.jsx
+++ React-task1/src/assets/components/FoodCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function FoodCard({name, description, price, image}) {
   return (
     <div className='foodcard'>
-        <img src="{image}" className='card-image'/>
+        <img src={image} className='card-image'/>
         <h2>{name}</h2>
         <p className='description'>{description}</p>
         <p className='card-price'>{price} AZN</p>
